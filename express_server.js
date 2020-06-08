c31733f49b1d22872535a9533390f85a7d43a2bd
Charlie Hayes <cjhayestutor@gmail.comgit config --global user.email cjhayestutor@gmail.com>
2020-06-08 23:55:35 +0000

    Added body parser NPM to express_server

--- express_server.js
+++ express_server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 const urlDatabase = {
