8957aae9e7e76cc7fe86858fcb99c5a837933cd2
Charlie Hayes <cjhayestutor@gmail.comgit config --global user.email cjhayestutor@gmail.com>
2020-06-08 20:14:22 +0000

    Setup ejs as the view engine for the project.

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
+
 const express = require("express");
 const app = express();
 const PORT = 8080;
+app.set("view engine", "ejs");
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -10,6 +13,15 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
